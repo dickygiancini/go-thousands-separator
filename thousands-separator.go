1c5959065fb504af0303456d9a935fc2ab1b4bf2
Dicky Giancini <dicky.giancini.123@gmail.com>
2025-03-13 12:01:03 +0700

    Perfecting the function

--- thousands-separator.go
+++ thousands-separator.go
@@ -1,7 +1,27 @@
 package go_thousands_separator
 
-func ThousandsSeparator(value int) string {
-	var result string = ""
+import (
+	"strconv"
+	"strings"
+)
 
-	return result
+func ThousandsSeparator(value int) string {
+	var result strings.Builder
+
+	s := strconv.Itoa(value)
+	n := len(s)
+
+	if n < 3 {
+		return s
+	}
+
+	for i := 0; i < n; i++ {
+		result.WriteByte(s[i])
+
+		if (n-i-1)%3 == 0 && i != n-1 {
+			result.WriteByte('.')
+		}
+	}
+
+	return result.String()
 }
